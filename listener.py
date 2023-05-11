e326cc242e2f2ba847c61404979dddef13b328fe
Amitai Shmeeda <amitaishmeeda@gmail.com>
2023-05-11 23:59:51 +0300

    more

--- listener.py
+++ listener.py
@@ -3,13 +3,25 @@ from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
 # Initialize a Slack API client with your app token
-client = WebClient(token='xoxb-5245319467909-5245510607157-FmgnMSyxya2AeAP1tMcPisa8')
+client = WebClient(token='xoxb-5245319467909-5245510607157-DWgT5kU53gIceAp0A7hUyXD3')
 
 # Specify the channel or conversation ID for which you want to extract the chat transcript
 channel_id = 'C0577A24AF7'
 
-# Make the API call to retrieve the conversation history
+# Specify the message you want to post
+message = 'Enter start to start bot listening'
+
+# Make the API call to post the message
 try:
+    response = client.chat_postMessage(
+        channel=channel_id,
+        text=message
+    )
+    print(f"BOT: {response['ts']}")
+except SlackApiError as e:
+    print(f"Error posting message: {e}")
+
+    # Make the API call to retrieve the conversation history
     response = client.conversations_history(channel=channel_id)
     messages = response['messages']
     # Process the messages or save them to a file
